96aa20c3c9fe9e8a8c42afc1a9caabdcc797d701
Vincent Xu <vincent.xu1@outlook.com>
2025-07-09 10:24:02 -0700

    Completed assignment 1.3.1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            UserPrompt();
+
+            string userInput = Console.ReadLine();
+            bool loop = true;
+
+            while (loop)
+            {
+                switch(userInput)
+                {
+                    case "a":
+                    case "b":
+                    case "c":
+                        loop = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please enter a valid option:");
+                        UserPrompt();
+                        userInput = Console.ReadLine();
+                        break;
+                }
+            }
+            
+            // User input error handling intentionally omitted. Just wanted to try tuples.
+            Console.WriteLine("Please enter figure dimensions:");
+            (double length, double width) dimensions = (Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Area is: ");
+            switch(userInput)
+            {
+                case "a":
+                    Console.WriteLine(AreaTriangle(dimensions.length, dimensions.width));
+                    break;
+                case "b":
+                    Console.WriteLine(AreaSquare(dimensions.length, dimensions.width));
+                    break;
+                case "c":
+                    Console.WriteLine(AreaRectangle(dimensions.length, dimensions.width));
+                    break;
+            }
+        }
+
+        static void UserPrompt()
+        {
+            Console.WriteLine("Please select a figure:");
+            Console.WriteLine("\ta) triangle");
+            Console.WriteLine("\tb) square");
+            Console.WriteLine("\tc) rectangle");
+        }
+
+        static double AreaSquare(double sideA, double sideB)
+        {
+            return AreaRectangle(sideA, sideB);
+        }
+
+        static double AreaRectangle(double length, double width)
+        {
+            return length * width;
+        }
+
+        static double AreaTriangle(double triangleBase, double height)
+        {
+            return 0.5 * triangleBase * height;
         }
     }
 }
